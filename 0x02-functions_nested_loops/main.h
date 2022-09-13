94adfe343da10639242d331dd64d3ebea7f4fc1f
KupakwasheMboho <mboho.kupa@gmail.com>
2022-09-13 16:55:57 -0700

    task 0

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,17 +1,7 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef HOLBERTON_H_INCLUDED
+#define HOLBERTON_H_INCLUDED
 
-#include <stdio.h>
-
-/**
- * main - MAIN_H
- *
- * Description: code will be used in other tasks
- *
- * Return: 0
- */
-
-int _putchar(char c);
+int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -21,8 +11,9 @@ int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
-int add(int, int);
+int add(int a, int b);
 void print_to_98(int n);
 void print_times_table(int n);
+void num(int r, int n);
 
-#endif /* MAIN_H */
+#endif
