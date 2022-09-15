14b9d5b9b2901ef8412276afddbce97834822701
KupakwasheMboho <mboho.kupa@gmail.com>
2022-09-15 16:59:29 -0700

    task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,23 +8,35 @@
 
 int main(void)
 {
-	int num;
+	int num = 1;
 
-	for (num = 1; num <= 100; ++num)
+	while (num++ < 100)
 	{
-		if (num % 3 == 0 && !(num % 5 == 0))
-			printf("Fizz");
-		else if (num % 5 == 0 && !(num % 3 == 0))
-			printf("Buzz");
-		else if (num % 3 == 0 && num % 5 == 0)
-			printf("FizzBuzz");
-		else	
-			printf("%d", num);
-
-		if (num != 100)
-			printf(" ");
+		if ((num % 3 == 0) && (num % 5 == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else if ((num % 3) == 0)
+		{
+			printf("Fizz ");
+		}
+		else if ((num % 5) == 0)
+		{
+			if (num != 100)
+			{
+				printf("Buzz ");
+			}
+			else
+			{
+				printf("Buzz");
+			}
+		}
 		else
-			printf("\n");
+		{
+			printf("%d ", num);
+		}
 	}
+	printf("\n");
+
 	return (0);
 }
