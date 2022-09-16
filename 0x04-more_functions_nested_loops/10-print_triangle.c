8cbecfbec435e4ce83997854fdcad8da11edeb15
KupakwasheMboho <mboho.kupa@gmail.com>
2022-09-15 17:06:21 -0700

    task 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include"main.h"
+
+/**
+ * print_triangle - prints a triangle
+ * @size:size parameter of triangle
+ * Return: returns nothing
+ */
+
+void print_triangle(int size)
+{
+	int hght, base;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (hght = 1; hght <= size; ++hght)
+		{
+			for (base = 1; base <= size; ++base)
+			{
+				if ((hght + base) <= size)
+					_putchar(' ');
+				else
+					_putchar('#');
+			}
+		}
+	}
+}
