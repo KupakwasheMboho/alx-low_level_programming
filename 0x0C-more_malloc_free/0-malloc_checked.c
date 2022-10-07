4031b936f582aac527bc84111d4d1294b6d8008b
KupakwasheMboho <mboho.kupa@gmail.com>
2022-10-06 17:17:24 -0700

    task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory insong malloc
+ * @b: memory to allocate
+ *
+ * Return: pointer t allocateed memomry
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *pp= malloc(b);
+	if (p == NULL)
+		exit(98);
+	return (p);
+}
